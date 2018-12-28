34d0b8c743f9232c50be95ccad1e1d5543dc429a
Jordan England-Nelson <jordan.england.nelson@gmail.com>
2018-12-28 12:45:26 -0800

    Updates

---
+++ precache-manifest.b2990839721b7f1d17ad2ee898c456e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b366f786033800777a4",
+    "url": "/portfolio-site-simple/static/js/runtime~main.6b366f78.js"
+  },
+  {
+    "revision": "5d36c759fd9f8b71268c",
+    "url": "/portfolio-site-simple/static/js/main.5d36c759.chunk.js"
+  },
+  {
+    "revision": "18dc06195f66c61097c2",
+    "url": "/portfolio-site-simple/static/js/1.18dc0619.chunk.js"
+  },
+  {
+    "revision": "5d36c759fd9f8b71268c",
+    "url": "/portfolio-site-simple/static/css/main.1d1f8834.chunk.css"
+  },
+  {
+    "revision": "18dc06195f66c61097c2",
+    "url": "/portfolio-site-simple/static/css/1.d7fdbb5e.chunk.css"
+  },
+  {
+    "revision": "6ee10989d1819f465a0664206238100c",
+    "url": "/portfolio-site-simple/index.html"
+  }
+];
