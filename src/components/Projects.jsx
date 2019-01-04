59ebd9a366bf0ec5ad08f265b78fe18fbd5adc5d
Jordan England-Nelson <jordan.england.nelson@gmail.com>
2019-01-03 21:43:48 -0800

    Adds IEP Goal Tracker images and lines them up next to the description

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './Projects.scss'
+import evaluations from '../img/evaluations_left.png';
+import login from '../img/login_center.png';
+import students from '../img/my_students_right.png'
 
 
 class Projects extends React.Component {
@@ -10,8 +13,18 @@ class Projects extends React.Component {
 
   render () {
     return (
-      <div>
-        Recent Projects
+      <div className="projects">
+        <div className="section-title">Recent Projects</div>
+        <div className="iep-goal-tracker">
+          <div className="images">
+            <div className="phone evaluations"/>
+            <div className="phone login"/>
+            <div className="phone students"/>
+          </div>
+          <p className="description">
+            <strong>IEP Goal Tracker</strong> is a web app that helps teachers of students with special needs who must comply with state-mandated Individualized Education Plans.
+          </p>
+        </div>
       </div>
     );
   }
