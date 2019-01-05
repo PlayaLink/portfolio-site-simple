0b124bee56388b6380429650d01ae1a79c1441b9
Jordan England-Nelson <jordan.england.nelson@gmail.com>
2019-01-04 19:38:37 -0800

    Adds Street Art Talks to project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -19,10 +19,31 @@ class Projects extends React.Component {
           </div>
           <p className="description">
             <strong>IEP Goal Tracker</strong> is a web app that helps teachers of students with
-            special needs who must comply with state-mandated Individualized Education Plans.
+            special needs by digitizing the paper evaluation forms used for tracking progress on state-enforced Individualized Education Plans (IEPs).
           </p>
           <a
             href="https://medium.com/@jordanenelson/a-goal-tracking-app-for-students-with-special-needs-6fe5334d2a53"
+            target="_blank"
+            className="button">Learn More</a>
+        </div>
+        <div className="street-art-talks">
+          <div className="images">
+            <div className="phone influence" />
+            <div className="phone street-art-talks" />
+            <div className="phone peace" />
+          </div>
+          <p className="description">
+            <strong>Street Art Talks</strong> is a web app that bridges the gap between (analog)
+            street art and (digital) information about that art work.
+            <br/>
+            <br/>
+            <div>Stencils tagged near prominent
+              murals in DTLA's arts district instruct passersby to text a code to a Twilio phone
+              number that will respond with a url that contains information about the mural and
+              artist.</div>
+          </p>
+          <a
+            href="http://www.streetarttalks.com/"
             target="_blank"
             className="button">Learn More</a>
         </div>
