3885501070e1de0403a8c1a2eed949191d77029c
Jordan England-Nelson <jordan.england.nelson@gmail.com>
2019-11-24 13:40:56 -0800

    Adds Meetly into the mix

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,53 +1,60 @@
-import React from 'react';
-import './ProjectCard.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./ProjectCard.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
 
-
-const ProjectCard = (props) => (
+const ProjectCard = props => (
   <div className={`project-card`}>
-    <div className="title">
-      { props.title }
-    </div>
+    <div className="title">{props.title}</div>
     <div className="subtitle">
-      { props.subtitle }
-      <hr className="divider"></hr>
+      {props.subtitle}
+      <hr className="divider" />
     </div>
-    <div className="description">
-      { props.description }
-    </div>
-    { (props.cta && props.download) && (
-      <div style={ { marginTop: '2rem' } }>
+    <div className="description">{props.description}</div>
+    {props.cta && props.download && (
+      <div style={{ marginTop: "2rem" }}>
         <a
-          href={ props.download }
+          href={props.download}
           download
           id={props.id}
-          className="button pink download-button">
-          {props.cta}<span
-          style={ {
-            position: 'relative',
-            marginRight: '10px',
-            marginLeft: '5px'
-          } }><FontAwesomeIcon icon="download" /></span>
+          className="button pink download-button"
+        >
+          {props.cta}
+          {!props.isDownload && (
+            <span
+              style={{
+                position: "relative",
+                marginRight: "10px",
+                marginLeft: "5px"
+              }}
+            >
+              <FontAwesomeIcon icon="download" />
+            </span>
+          )}
         </a>
       </div>
-    ) }
-    { (props.cta && props.link) && (
-      <div style={ { marginTop: '2rem' } }>
+    )}
+    {props.cta && props.link && (
+      <div style={{ marginTop: "2rem" }}>
         <a
-          href={ props.link }
+          href={props.link}
           id={props.id}
           target="_blank"
-          className="button pink download-button">
-          {props.cta}<span
-          style={ {
-            position: 'relative',
-            marginRight: '10px',
-            marginLeft: '5px'
-          } }><FontAwesomeIcon icon="arrow-right" /></span>
+          className="button pink download-button"
+        >
+          {props.cta}
+          <span
+            style={{
+              position: "relative",
+              marginRight: "10px",
+              marginLeft: "5px"
+            }}
+          >
+            <FontAwesomeIcon icon="arrow-right" />
+          </span>
         </a>
       </div>
-    ) }
+    )}
   </div>
 );
 
