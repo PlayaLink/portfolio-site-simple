7792369534d98509b5c7089f342494b356c766d9
Jordan England-Nelson <jordan.england.nelson@gmail.com>
2020-02-20 12:57:59 -0700

    Show email when hovering the email icon

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,41 +3,58 @@ import "./About.scss";
 import Timeline from "./Timeline";
 import { DirectLink } from "react-scroll";
 import jordan_portrait from "../img/jordan_portrait.jpg";
-import tall_t from "../img/tall_t.svg";
+import email_icon from "../img/email_icon.svg";
+import my_email from "../img/my_email.svg";
 import short_t from "../img/short_t.svg";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-
-const SocialButtons = () => (
-  <div className="header">
-    <div className="icon-list">
-      <a
-        className="icon linkedin"
-        href="https://www.linkedin.com/in/jordan-england-nelson-05283494/"
-        target="_blank"
-      />
-      <a
-        className="icon twitter"
-        href="https://twitter.com/jordanenelson"
-        target="_blank"
-      />
-      {/*<a className="icon github" href="https://github.com/PlayaLink" target="_blank"></a>*/}
-    </div>
-    <div className="tagline">
-      {/*<p>I'm a <strong>product developer</strong> with broad startup experience in tech.</p>*/}
-      {/*<p>I try to <span className="blue">understand</span> people, so I can <span className="red">design</span> things they need, and <span className="teal">build</span> things they will use.</p>*/}
-      {/*<div>I try to <span className="blue">understand</span> people</div>*/}
-      {/*<div>So I can <span className="red">design</span> things they need</div>*/}
-      {/*<div>And <span className="teal">build</span> things they will use.</div>*/}
-      {/*<p style={{textAlign: 'left'}}>I try to understand people</p>*/}
-      {/*<p style={{textAlign: 'center'}}>So I can design things they need</p>*/}
-      {/*<p style={{textAlign: 'right'}}>And build things they will use.</p>*/}
-      {/*<p style={{textAlign: 'left'}}>I try to <span className="blue">understand</span> people</p>*/}
-      {/*<p style={{textAlign: 'center'}}>So I can <span className="red">design</span> things they need</p>*/}
-      {/*<p style={{textAlign: 'right'}}>And <span className="teal">build</span> things they will use.</p>*/}
-    </div>
-  </div>
-);
+class SocialButtons extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showEmail: false
+    };
+  }
+  toggleEmail = () => {
+    this.setState({ showEmail: !this.state.showEmail})
+  }
+  render() {
+    const { showEmail } = this.state;
+    return (
+      <div className="header">
+        <div className="icon-list">
+          <a
+            className="icon linkedin"
+            href="https://www.linkedin.com/in/jordan-england-nelson-05283494/"
+            target="_blank"
+          />
+          <a
+            className="icon twitter"
+            href="https://twitter.com/jordanenelson"
+            target="_blank"
+          />
+          <div>
+            <img onMouseEnter={this.toggleEmail} onMouseLeave={this.toggleEmail} src={showEmail ? my_email : email_icon} />
+          </div>
+          {/*<a className="icon github" href="https://github.com/PlayaLink" target="_blank"></a>*/}
+        </div>
+        <div className="tagline">
+          {/*<p>I'm a <strong>product developer</strong> with broad startup experience in tech.</p>*/}
+          {/*<p>I try to <span className="blue">understand</span> people, so I can <span className="red">design</span> things they need, and <span className="teal">build</span> things they will use.</p>*/}
+          {/*<div>I try to <span className="blue">understand</span> people</div>*/}
+          {/*<div>So I can <span className="red">design</span> things they need</div>*/}
+          {/*<div>And <span className="teal">build</span> things they will use.</div>*/}
+          {/*<p style={{textAlign: 'left'}}>I try to understand people</p>*/}
+          {/*<p style={{textAlign: 'center'}}>So I can design things they need</p>*/}
+          {/*<p style={{textAlign: 'right'}}>And build things they will use.</p>*/}
+          {/*<p style={{textAlign: 'left'}}>I try to <span className="blue">understand</span> people</p>*/}
+          {/*<p style={{textAlign: 'center'}}>So I can <span className="red">design</span> things they need</p>*/}
+          {/*<p style={{textAlign: 'right'}}>And <span className="teal">build</span> things they will use.</p>*/}
+        </div>
+      </div>
+    );
+  }
+}
 
 const Summary = () => (
   <div className="summary">
@@ -102,21 +119,20 @@ class About extends React.Component {
               src={jordan_portrait}
               alt="Jordan England-Nelson"
               className="rounded-lg float-left mr-3 d-md-none"
-              style={{width: "45%"}}
+              style={{ width: "45%" }}
             />
             <p>
-
-                <span className="" style={{ fontSize: "1.5rem" }}>
-                  I never identified as a technical person.
-                </span>{" "}
-              Until I realized JavaScript is just a language – a way to
-              transmit ideas from one system to another.
+              <span className="" style={{ fontSize: "1.5rem" }}>
+                I never identified as a technical person.
+              </span>{" "}
+              Until I realized JavaScript is just a language – a way to transmit
+              ideas from one system to another.
             </p>
             <p>
               Design, too, is a language. We listen and observe people. We try
               to interpret their needs, goals and behaviors. We translate what
-              they say and do into what we think they mean and want, so that
-              our response – our product – is relevant and helpful.
+              they say and do into what we think they mean and want, so that our
+              response – our product – is relevant and helpful.
             </p>
             {/*<p>*/}
             {/*I know<span className="font-italic">&nbsp;generalist</span> is a*/}
@@ -131,8 +147,9 @@ class About extends React.Component {
             {/*</p>*/}
             <p>
               My experience in product blurs the lines between research,
-              development and design. I'm a generalist, yes, but also a translator – of user needs into
-              product ideas, of ideas into designs, and of designs into code.
+              development and design. I'm a generalist, yes, but also a
+              translator – of user needs into product ideas, of ideas into
+              designs, and of designs into code.
             </p>
             <div>
               <SocialButtons />
