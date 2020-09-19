606440fd280d0c89360e9003b16dc228a22d752c
Jordan England-Nelson <jordan.england.nelson@gmail.com>
2020-09-19 12:50:58 -0600

    Updates the Design Research and Observations sections

--- src/components/case_studies/Meetly.jsx
+++ src/components/case_studies/Meetly.jsx
@@ -1,24 +1,17 @@
-import React, { Component } from "react";
-import add_item_and_share from "../../img/add_item_and_share.mp4";
-import calendar_integration from "../../img/calendar_integration.mp4";
-import add_remove_cards from "../../img/add_remove_cards.mp4";
-import choose_template from "../../img/choose_template.mp4";
-import meeting_groups from "../../img/meeting_groups.mp4";
-import search from "../../img/search.mp4";
-import { Waypoint } from "react-waypoint";
+import React, { Component } from 'react';
+import add_item_and_share from '../../img/add_item_and_share.mp4';
+import calendar_integration from '../../img/calendar_integration.mp4';
+import add_remove_cards from '../../img/add_remove_cards.mp4';
+import choose_template from '../../img/choose_template.mp4';
+import meeting_groups from '../../img/meeting_groups.mp4';
+import search from '../../img/search.mp4';
+import { Waypoint } from 'react-waypoint';
+import 'react-vertical-timeline-component/style.min.css';
+import { Tooltip } from 'react-tippy';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import {
-  VerticalTimeline,
-  VerticalTimelineElement
-} from "react-vertical-timeline-component";
-import "react-vertical-timeline-component/style.min.css";
-import { Tooltip } from "react-tippy";
-import styled, { css } from "styled-components";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import ShowMore from "../widgets/ShowMore";
-import { Link } from "react-router-dom";
-
-const iconStyle = { background: "rgb(33, 150, 243)", color: "#fff" };
+const iconStyle = { background: 'rgb(33, 150, 243)', color: '#fff' };
 
 const Dot = styled.span`
   height: 0.5rem;
@@ -33,9 +26,10 @@ const TimelineItem = props => (
   <li className="mb-3">
     <div className="d-flex">
       <div className="col-3 text-uppercase pr-0 pl-1 font-weight-normal">
-        {props.date}
-      </div>{" "}
-      <div className="col pl-2 font-weight-light">{props.description}</div>
+        { props.date }
+      </div>
+      { ' ' }
+      <div className="col pl-2 font-weight-light">{ props.description }</div>
     </div>
   </li>
 );
@@ -50,38 +44,36 @@ const MeetlyIntro = () => (
         <h1 className="display-4">
           Meetly
         </h1>
-        <h2 className="display-4 text-muted">
-          Turning an idea into a fully fledged product
-        </h2>
-        <h5 className="font-weight-light mt-4">
-          Meetly is a <span className="font-weight-bold">calendar automation tool</span> that grew out of user research I conducted at POPin, my former employer.
-        </h5>
-        <h5 className="font-weight-light mt-4">
-          Three colleagues and I left POPin to turn the idea into a stand along product.
-        </h5>
+        <h3 className="text-muted font-weight-light">
+          How design research lead to the creation of a new company
+        </h3>
+        { /*<h5 className="font-weight-light mt-4">*/ }
+        { /*Meetly is a <span className="font-weight-bold">calendar automation tool</span> that grew out of user research I conducted at POPin, my former employer.*/ }
+        { /*</h5>*/ }
+        { /*<h5 className="font-weight-light mt-4">*/ }
+        { /*Three colleagues and I left POPin to turn the idea into a stand along product.*/ }
+        { /*</h5>*/ }
       </div>
     </div>
     <div className="row d-flex justify-content-center mt-5">
       <div className="col-12 col-md-8">
-        <PlayOnScroll id="meetly-overview" video={add_item_and_share} />
+        <PlayOnScroll id="meetly-overview" video={ add_item_and_share } />
       </div>
     </div>
   </div>
 );
 
 const MeetlyBackground = () => (
-  <div className="bg-white mt-5">
+  <div className="bg-white">
     <div className="d-flex justify-content-center">
-      <div className="col-12 col-md-6">
-        <h5 className="text-uppercase text-muted mt-5 mb-3">Context</h5>
-        <h2 className="text-uppercase">How Meetly got started</h2>
+      <div className="col-12 col-md-6  mt-5">
+        <h2 className="text-uppercase">How Meetly started</h2>
         <p className="font-weight-light">
-          The idea for Meetly came as operations were winding down at POPin, an
-          Irvine, CA-based startup that made employee engagement software.
+          Meetly is a <span className="font-weight-bold">calendar automation tool</span> that grew
+          out of user research I conducted at POPin, my former employer.
         </p>
         <p className="font-weight-light">
-          Our team approached POPin's largest investor and secured seed money to
-          develop the idea into a product.
+          Three colleagues and I left POPin to turn the idea into a stand along product.
         </p>
       </div>
     </div>
@@ -113,8 +105,8 @@ const MeetlyBackground = () => (
     <div className="d-flex justify-content-center mt-5 pb-5">
       <div className="col-12 col-md-6">
         <h4 className="text-uppercase">Timeline</h4>
-        {/*<h5 className="font-weight-light">Most of the greenfield design research occured in spring of 2019.</h5>*/}
-        {/*<h5 className="font-weight-light">In June 2019, our team of four left POPin to develop Meetly.</h5>*/}
+        { /*<h5 className="font-weight-light">Most of the greenfield design research occured in spring of 2019.</h5>*/ }
+        { /*<h5 className="font-weight-light">In June 2019, our team of four left POPin to develop Meetly.</h5>*/ }
         <ul className="ml-4 mt-4">
           <TimelineItem
             date="Spring 2019"
@@ -141,33 +133,42 @@ const MeetlyResearch = () => (
   <div className="bg-light">
     <div className="d-flex justify-content-center mt-5">
       <div className="col-12 col-md-6">
-        <h5 className="text-uppercase text-muted mb-4">Research</h5>
-        <h2 className="text-uppercase">Process</h2>
+        <h2 className="text-uppercase">Design Research</h2>
         <div className="font-weight-light">
-          I lead 13 interviews with executive assistants, project managers and
-          department leaders (from HR, IT and sales) to learn about:
-          <ol className="ml-4">
+          <p>While interviewing and observing users of POPin, an employee engagement tool, we
+            discovered that people were using our tool to <span className="font-weight-bold">solve a problem</span> we
+            had not focused on:
+            time wasted in unnecessary
+            or <span className="font-weight-bold">unproductive meetings</span>.</p>
+          <p>Recognizing an opportunity,
+            our <span className="font-weight-bold">research objectives</span> changed to:</p>
+          <ul className="ml-4">
             <li className="my-3">
-              how they schedule, conduct and document their meetings and
+              what is a successful meeting
             </li>
             <li className="my-3">
-              the tools they used to do their jobs, specifically tools that
-              touched their meetings workflow.
+              what are the impediments to a successful meeting
             </li>
-          </ol>
+            <li className="my-3">
+              how are people solving these challenges
+            </li>
+            <li className="my-3">
+              what tools are they using
+            </li>
+          </ul>
         </div>
-        <p className="font-weight-light">
-          Tested out a host of productivity SaaS products focused on project
-          management, note-taking, and knowledge-base creation.
-        </p>
         <h2 className="text-uppercase mt-5">Observations</h2>
         <ul className="ml-4 font-weight-light">
-          <li className="my-3">Difficult to enforce proper meeting hygiene</li>
+          <li className="my-3">
+            <div className="d-flex flex-column">
+              <span>Difficult to enforce proper meeting hygiene</span>
+              <span>e.g. every meeting should have an agenda and action items</span></div>
+          </li>
           <li className="my-3">
             Lack of accountability around tasks assigned / decisions made
           </li>
           <li className="my-3">
-            Existing solutions are{" "}
+            Existing solutions are{ ' ' }
             <Tooltip
               // options
               title="Asana, Monday, Confluence, etc"
@@ -176,8 +177,8 @@ const MeetlyResearch = () => (
               className="text-primary"
             >
               too complex
-            </Tooltip>{" "}
-            or too{" "}
+            </Tooltip>{ ' ' }
+            or too{ ' ' }
             <Tooltip
               // options
               title="Google Docs, Trello"
@@ -248,9 +249,10 @@ const NoExtraWork = () => (
             called "workspaces", for each of your meetings.
           </li>
           <li className="my-3">
-            A workspaces&nbsp;<em className="font-weight-bold">stays in sync</em>{" "}
+            A workspaces&nbsp;<em className="font-weight-bold">stays in sync</em>{ ' ' }
             with its corresponding calendar event - if you change the meeting
-            name, time or guest list, those changes are reflected in Meetly. This was technically difficult and <em className="font-weight-bold">no other notes tools does this.</em>
+            name, time or guest list, those changes are reflected in Meetly. This was technically
+            difficult and <em className="font-weight-bold">no other notes tools does this.</em>
           </li>
           <li className="my-3">
             Individual workspaces are accessible directly from a user's calendar, and vice versa.
@@ -267,7 +269,7 @@ const NoExtraWork = () => (
           the notes for that meeting with one click.
         </span>
       }
-      video={calendar_integration}
+      video={ calendar_integration }
     />
   </div>
 );
@@ -283,16 +285,17 @@ class PlayOnScroll extends React.Component {
     const { id } = this.props;
     document.getElementById(id).load();
   };
-  render() {
+
+  render () {
     const { video, id } = this.props;
     return (
-      <Waypoint onEnter={this.startVideo} onLeave={this.pauseVideo}>
+      <Waypoint onEnter={ this.startVideo } onLeave={ this.pauseVideo }>
         <video
-          className="img-fluid my-1 my-md-3"
-          src={video}
-          loop={true}
+          className="img-fluid my-1 mb-md-5 mt-md-3"
+          src={ video }
+          loop={ true }
           controls
-          id={id}
+          id={ id }
           muted
         />
       </Waypoint>
@@ -301,15 +304,16 @@ class PlayOnScroll extends React.Component {
 }
 
 class VideoExample extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
     this.state = {
       autoPlay: false
     };
   }
+
   startVideo = () => {
     const { id } = this.props;
-    console.log("id: ", id);
+    console.log('id: ', id);
     const video = document.getElementById(id);
     video.currentTime = 0;
     video.play();
@@ -318,24 +322,25 @@ class VideoExample extends React.Component {
     const { id } = this.props;
     document.getElementById(id).pause();
   };
-  render() {
+
+  render () {
     const { title, body, video, reverse, id } = this.props;
     const { autoPlay } = this.state;
     return (
       <div
-        className={`d-md-flex justify-content-center mt-5 ${
-          reverse ? "flex-row-reverse" : ""
-        }`}
+        className={ `d-md-flex justify-content-center mt-5 ${
+          reverse ? 'flex-row-reverse' : ''
+          }` }
       >
         <div className="col-12 col-md-3">
           <h5 className="text-uppercase mt-5 mb-3">
-            {title}
+            { title }
           </h5>
-          {/*<h6 className="text-uppercase mb-2 mt-md-5"></h6>*/}
-          <p className="font-weight-light d-none d-md-block">{body}</p>
+          { /*<h6 className="text-uppercase mb-2 mt-md-5"></h6>*/ }
+          <p className="font-weight-light d-none d-md-block">{ body }</p>
         </div>
         <div className="col-12 col-md-8">
-          <PlayOnScroll video={video} id={id} />
+          <PlayOnScroll video={ video } id={ id } />
         </div>
       </div>
     );
@@ -346,7 +351,7 @@ const PrescriptiveButFlexible = () => (
   <div className="bg-white mt-5">
     <div className="d-flex justify-content-center">
       <div className="col-12 col-md-6">
-        <SectionTitle heading="Design Principle #2" title="Prescriptive, but flexible"/>
+        <SectionTitle heading="Design Principle #2" title="Prescriptive, but flexible" />
         <p className="font-weight-light">
           We wanted Meetly to be intuitive as a tool, but also "educational"
           when it comes to good meeting hygiene.
@@ -367,11 +372,11 @@ const PrescriptiveButFlexible = () => (
           default. This arrangement&nbsp;
           <em className="font-weight-bold">
             encourages meeting best practices
-          </em>{" "}
+          </em>{ ' ' }
           while allowing a user to remove, add or rename cards as needed.
         </span>
       }
-      video={add_remove_cards}
+      video={ add_remove_cards }
     />
     <VideoExample
       title="Meeting templates"
@@ -381,7 +386,7 @@ const PrescriptiveButFlexible = () => (
           Users can create custom card arrangements for different meeting types.
         </span>
       }
-      video={choose_template}
+      video={ choose_template }
     />
   </div>
 );
@@ -390,7 +395,7 @@ const EasyToReference = () => (
   <div className="bg-white mt-5">
     <div className="d-flex justify-content-center">
       <div className="col-12 col-md-6">
-        <SectionTitle heading="Design Principle #3" title="Easy to reference past notes"/>
+        <SectionTitle heading="Design Principle #3" title="Easy to reference past notes" />
         <p className="font-weight-light">
           A common complaint we heard was that meeting minutes get shared via
           email, often as attachments, which makes it difficult to reference
@@ -407,7 +412,7 @@ const EasyToReference = () => (
           reference past topics.
         </span>
       }
-      video={search}
+      video={ search }
     />
     <VideoExample
       title="Meeting Groups"
@@ -418,27 +423,27 @@ const EasyToReference = () => (
           is another way users could organize and quickly reference content
         </span>
       }
-      video={meeting_groups}
+      video={ meeting_groups }
     />
   </div>
 );
 
 const SectionTitle = (props) => {
-  const {heading, title} = props;
+  const { heading, title } = props;
   return (
     <React.Fragment>
       <h5 className="text-uppercase text-muted mt-5 mb-3">
-        {heading}
+        { heading }
       </h5>
       <h2 className="text-uppercase">
-        {title}
+        { title }
       </h2>
     </React.Fragment>
-  )
-}
+  );
+};
 
 class Meetly extends Component {
-  render() {
+  render () {
     return (
       <div className="">
         <MeetlyIntro />
